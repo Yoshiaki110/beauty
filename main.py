265957e7c6c4d7b5ae0e8d42c932f7a835634de0
yoshiaki110 <yoshiaki110@gmail.com>
2021-01-23 13:30:54 +0900

    最初のコミット

---
+++ main.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, abort, render_template, send_file, Response, jsonify, make_response
+from flask_cors import CORS
+from flask_socketio import SocketIO
+import threading
+
+import os
+import json
+import uuid
+import time
+import glob
+import shutil
+from zipfile import ZipFile
+
+
+app = Flask(__name__)
+CORS(app)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False     # jsonifyでのエラーを抑止するため
+socketio = SocketIO(app, async_mode=None)
+#postConfirm = True
+
+# 環境変数取得
+SKYWAY_APIKEY = os.environ["SKYWAY_APIKEY"]
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/adviser')
+def adviser():
+    return render_template('video.html', apikey=SKYWAY_APIKEY, peerid='adviser')
+
+@app.route('/user')
+def user():
+    return render_template('video.html', apikey=SKYWAY_APIKEY, peerid='user')
+
+@app.route("/api/answer", methods=['POST'])
+def answer():
+    return jsonify({})
+
+
+# Flaskを使用してサーバーを起動
+if __name__ == "__main__":
+    port = int(os.getenv("PORT"))
+    socketio.run(app, host="0.0.0.0", port=port)
+
