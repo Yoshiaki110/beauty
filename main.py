ac8c48cebaa60f6310343eacfaa342cefa9e3dff
yoshiaki110 <yoshiaki110@gmail.com>
2021-02-07 15:28:05 +0900

    debug

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from flask import Flask, request, abort, render_template, send_file, Response, j
 from flask_cors import CORS
 from flask_socketio import SocketIO
 import threading
-import base64
+#import base64
 
-from face_detect import get_facepos
+#from face_detect import get_facepos
 import os
 import json
 import uuid
@@ -17,8 +17,8 @@ from zipfile import ZipFile
 app = Flask(__name__)
 CORS(app)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False     # jsonifyでのエラーを抑止するため
-app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024
-app.config['UPLOAD_FOLDER'] = './tmp'
+#app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024
+#app.config['UPLOAD_FOLDER'] = './tmp'
 socketio = SocketIO(app, async_mode=None)
 #postConfirm = True
 
@@ -50,6 +50,7 @@ def oshaku():
     #return render_template('oshaku.html', apikey=SKYWAY_APIKEY, serve=serve, aikotoba=aikotoba)
     return render_template('aioshaku.html')
 
+'''
 @app.route("/ehon", methods=['GET'])
 def ehon():
     return render_template('ehon.html')
@@ -62,6 +63,7 @@ def face():
     img.save(path)
     face_pos = get_facepos(path)
     return jsonify(face_pos)
+'''
 
 @app.route("/api/answer", methods=['POST'])
 def answer():
